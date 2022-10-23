be00befe14faa59352f0c8490e388b35e6af7cd9
duckyfuz <kennethgao25@gmail.com>
2022-10-23 10:24:17 +0800

    Got sessions to work

--- helpers.py
+++ helpers.py
@@ -9,25 +9,62 @@ from functools import wraps
 from spotipy.oauth2 import SpotifyOAuth
 
 
+# Checks to see if token is valid and gets a new token if not
+def get_token(session):
+    token_valid = False
+    token_info = session.get("token_info", {})
+
+    # Checking if the session already has a token stored
+    if not (session.get('token_info', False)):
+        token_valid = False
+        return token_info, token_valid
+
+    # Checking if token has expired
+    now = int(time.time())
+    is_token_expired = session.get('token_info').get('expires_at') - now < 60
+
+    # Refreshing token if it has expired
+    if (is_token_expired):
+        # Don't reuse a SpotifyOAuth object because they store token info and you could leak user tokens if you reuse a SpotifyOAuth object
+        sp_oauth = spotipy.oauth2.SpotifyOAuth(client_id = CLI_ID, client_secret = CLI_SEC, redirect_uri = REDIRECT_URI, scope = SCOPE)
+        token_info = sp_oauth.refresh_access_token(session.get('token_info').get('refresh_token'))
+
+    token_valid = True
+    return token_info, token_valid
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 def login_to():
     """Login to SPOTIFY, retrive liked songs"""
     scope = "user-library-read user-read-private"
-    sp = spotipy.Spotify(auth_manager=SpotifyOAuth(client_id=cred.client_ID, client_secret= cred.client_SECRET, redirect_uri=cred.redirect_url, scope=scope))
+    sp = spotipy.Spotify(auth_manager=SpotifyOAuth(client_id=cred.client_ID, 
+                                                   client_secret= cred.client_SECRET, 
+                                                   redirect_uri=cred.redirect_url, 
+                                                   scope=scope))
     results = sp.current_user_saved_tracks(limit=40)
     for idx, item in enumerate(results['items']):
         track = item['track']
         print(idx, track['artists'][0]['name'], " – ", track['name'])
-    username = sp.me()
-    print(username)
+    
     return 
 
 def login_required(f):
-
     # This is from the archived code, write something to make sure that the user is logged in
-
     """
     Decorate routes to require login.
-
     https://flask.palletsprojects.com/en/1.1.x/patterns/viewdecorators/
     """
     @wraps(f)
